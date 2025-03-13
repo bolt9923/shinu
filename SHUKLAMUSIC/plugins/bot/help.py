780107cf66b6681bedfc8d6a83c7f6cffd0f06a3
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-13 13:04:49 +0530

    Update help.py

--- SHUKLAMUSIC/plugins/bot/help.py
+++ SHUKLAMUSIC/plugins/bot/help.py
@@ -61,7 +61,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                   "😎𝗣𝗔𝗛𝗟𝗘 𓆩𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛𓆪 𝗞𝗢 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 𝗝𝗔𝗞𝗘 😆😆", show_alert=True
+                   "😎𝗣𝗔𝗛𝗟𝗘 ➻ ⏤͟͞ 𝙆𝙄𝙍𝙈𝘼𝘿𝘼⏤͟͞ ┈➤ ⌯ ❰ #𝙇𝙊𝙍𝘿 ❱ 𝗞𝗢 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 𝗝𝗔𝗞𝗘 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
