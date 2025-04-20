0833a5bbf91cff4451fbdfc47b3717721bcfa9d4
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-04-20 21:05:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,7 +87,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/30mfzb.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/ms7xee.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/80e35d93784e239bdc86c.jpg"
